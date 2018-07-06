5829a45064dcc02c0ec0dd39424f69a18149a00e
Seamus Connolly <sghconnolly@gmail.com>
2018-07-06 13:00:38 +0100

    feat: Change function name

--- src/Location.js
+++ src/Location.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class Location extends React.Component {
 
-    showClick = () => {
+    handleClick = () => {
         console.log('Click!!')
     }
 
@@ -10,7 +10,7 @@ class Location extends React.Component {
         const { location } = this.props;
         // The location.id is over-ridden in filtered lists by updateList() in CreateList.js
         return(
-            <li key={location.title} onClick={this.showClick}>
+            <li key={location.title} onClick={this.handleClick}>
                 {location.id}.&nbsp;&nbsp;{location.title}
             </li>
         )
