6a16643784d1915776c5f59a6d2211ea5cddea72
Seamus Connolly <sghconnolly@gmail.com>
2018-07-06 07:34:12 +0100

    feat: Add a sidenav
    Has ability to reduce to a 'hamburger' menu.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,84 @@
 import React from "react";
 import "./index.css";
+import SideNav from './SideNav'
 
 export default class App extends React.Component {
+    state = {
+        sidebar: "sidenav-active",
+        locations: [
+            {
+                title: "Spinnaker Tower",
+                location: {
+                    lat: 50.79557519999999,
+                    lng: -1.1085171
+                }
+            },
+            {
+                title: "HMS Warrior 1860",
+                location: {
+                    lat: 50.7982384,
+                    lng: -1.1092475
+                }
+            },
+            {
+                title: "Round Tower",
+                location: {
+                    lat: 50.7905406,
+                    lng: -1.1088802
+                }
+            },
+            {
+                title: "Mary Rose Museum",
+                location: {
+                    lat: 50.8022114,
+                    lng: -1.1088526
+                }
+            },
+            {
+                title: "Action Stations",
+                location: {
+                    lat: 50.79991680000001,
+                    lng: -1.1070106
+                }
+            },
+            {
+                title: "Charles Dickens Birthplace Museum",
+                location: {
+                    lat: 50.8070676,
+                    lng: -1.0872127
+                }
+            },
+            {
+                title: "The D-Day Story",
+                location: {
+                    lat: 50.77964100000001,
+                    lng: -1.089412
+                }
+            },
+            {
+                title: "Southsea Castle",
+                location: {
+                    lat: 50.777995,
+                    lng: -1.0888283
+                }
+            },
+            {
+                title: "Royal Marines Museum",
+                location: {
+                    lat: 50.78429,
+                    lng: -1.053795
+                }
+            },
+            {
+                title: "The Royal Navy Submarine Museum",
+                location: {
+                    lat: 50.7881692,
+                    lng: -1.1195995
+                }
+            }
+        ]
+    }
+
     componentDidMount() {
         let map = new window.google.maps.Map(document.getElementById("map"), {
             center: {
@@ -29,6 +106,10 @@ export default class App extends React.Component {
         this.setState({ sidebar: elementClass });
     }
 
+    closeNav = () => {
+        this.setState({ sidebar: "sidenav" });
+    };
+
     render() {
         return (
             <div id="app">
@@ -38,6 +119,10 @@ export default class App extends React.Component {
                     </span>
                     <h1>Portsmouth POIs</h1>
                 </header>
+                <SideNav
+                    className={this.state.sidebar}
+                    close={this.closeNav}
+                />
                 <div id="map" />
             </div>
         );
---
+++ src/SideNav.js
@@ -0,0 +1,31 @@
+import React from 'react'
+//import CreateList from './CreateList'
+
+class SideNav extends React.Component {
+    render() {
+        let visibility = (this.props.className === 'sidenav') ? 'ul' : '.ul-active'
+        let className = this.props.className
+        let linkStyle = {
+            listStyle: "none"
+        }
+        return (
+            <nav className={className}>
+                <ul id="navUL" className={{visibility}} style={linkStyle}>
+                    <span><li className="closebtn" onClick={this.props.close}>&times;</li></span>
+
+                </ul>
+            </nav>
+        )
+    }
+}
+
+export default SideNav
+
+/*
+<CreateList
+    locations={this.props.locations}
+    markers={this.props.markers}
+    map={this.props.map}
+ />
+
+ */
